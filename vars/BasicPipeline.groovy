68194cb4b35986943519d3185afd3097da4f5d99
Av2 <vonnetworking2@gmail.com>
2020-02-01 20:12:05 -0500

    updated pipeline contents

--- vars/BasicPipeline.groovy
+++ vars/BasicPipeline.groovy
@@ -1,12 +1,18 @@
 def call(body) {
 
-  stage ("I'm a firestarter") {
-    sh "echo 'Im a firestarter'"
+  stage ("Build") {
+    sh "gradle clean build"
   }
-  stage ("Tuwisted firestarter") {
-    sh "echo 'Tuwisted firestarter'"
+  stage ("Unit Test") {
+    sh "echo 'Unit Tests would go here...'"
   }
-  stage ("The Prodigy") {
-    sh "echo 'Wuum wowowowowowow'"
+  stage ("Docker Build") {
+    def configMap = readYaml file:"pipeline_config.yaml"
+    def app_version = readFile file:"VERSION"
+    sh "docker image build -t springboot-test:$app_version ."
+  }
+  stage ("Docker Dev Push") {
+    def app_version = readFile file:"VERSION"
+    sh "LOCAL_IMAGE_ID=`docker images | grep springboot-test | awk '{print \$3}'` && docker tag \$LOCAL_IMAGE_ID vonnetworking/springboot-test:app-dev-$app_version && docker push vonnetworking/springboot-test"
   }
 }

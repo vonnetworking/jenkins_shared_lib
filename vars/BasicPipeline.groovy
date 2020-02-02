d29f4ed31ebb8046368335bdea5f39bb3e610dc6
Av2 <vonnetworking2@gmail.com>
2020-02-01 21:16:24 -0500

    fixing pathing on execs

--- vars/BasicPipeline.groovy
+++ vars/BasicPipeline.groovy
@@ -20,7 +20,7 @@ def call(body) {
     sh "LOCAL_IMAGE_ID=`/usr/local/bin/docker images | grep springboot-test | awk '{print \$3}'` && /usr/local/bin/docker tag \$LOCAL_IMAGE_ID vonnetworking/springboot-test:app-dev-$app_version"
 
     withCredentials([usernamePassword(credentialsId: 'av_dockerhub_id', usernameVariable: 'HUB_USER', passwordVariable: 'HUB_PASS')]) {
-      sh "/usr/local/bin/docker login -u \$HUB_USER -p \$HUB_PASS"
+      sh "export PATH=$PATH:/usr/local/bin && /usr/local/bin/docker login -u \$HUB_USER -p \$HUB_PASS"
     }
 
     sh "/usr/local/bin/docker push vonnetworking/springboot-test"

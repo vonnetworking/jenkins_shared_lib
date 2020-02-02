c0b17cd46efd88267aaac516d482bff84fb1126e
Av2 <vonnetworking2@gmail.com>
2020-02-01 20:18:56 -0500

    added absolute paths to execs

--- vars/BasicPipeline.groovy
+++ vars/BasicPipeline.groovy
@@ -1,7 +1,7 @@
 def call(body) {
 
   stage ("Build") {
-    sh "gradle clean build"
+    sh "/usr/local/bin/gradle clean build"
   }
   stage ("Unit Test") {
     sh "echo 'Unit Tests would go here...'"
@@ -9,10 +9,10 @@ def call(body) {
   stage ("Docker Build") {
     def configMap = readYaml file:"pipeline_config.yaml"
     def app_version = readFile file:"VERSION"
-    sh "docker image build -t springboot-test:$app_version ."
+    sh "/usr/local/bin/docker image build -t springboot-test:$app_version ."
   }
   stage ("Docker Dev Push") {
     def app_version = readFile file:"VERSION"
-    sh "LOCAL_IMAGE_ID=`docker images | grep springboot-test | awk '{print \$3}'` && docker tag \$LOCAL_IMAGE_ID vonnetworking/springboot-test:app-dev-$app_version && docker push vonnetworking/springboot-test"
+    sh "LOCAL_IMAGE_ID=`/usr/local/bin/docker images | grep springboot-test | awk '{print \$3}'` && /usr/local/bin/docker tag \$LOCAL_IMAGE_ID vonnetworking/springboot-test:app-dev-$app_version && /usr/local/bin/docker push vonnetworking/springboot-test"
   }
 }

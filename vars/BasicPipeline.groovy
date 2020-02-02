0fbf44fec3034c79ff2af6b028660e3a61b7b6ea
Av2 <vonnetworking2@gmail.com>
2020-02-01 21:23:00 -0500

    updated publish steps and added cleanup steps at the end

--- vars/BasicPipeline.groovy
+++ vars/BasicPipeline.groovy
@@ -20,10 +20,13 @@ def call(body) {
     sh "LOCAL_IMAGE_ID=`/usr/local/bin/docker images | grep springboot-test | awk '{print \$3}'` && /usr/local/bin/docker tag \$LOCAL_IMAGE_ID vonnetworking/springboot-test:app-dev-$app_version"
 
     withCredentials([usernamePassword(credentialsId: 'av_dockerhub_id', usernameVariable: 'HUB_USER', passwordVariable: 'HUB_PASS')]) {
-      sh "export PATH=$PATH:/usr/local/bin && /usr/local/bin/docker login -u \$HUB_USER -p \$HUB_PASS"
+      sh "export PATH=$PATH:/usr/local/bin && /usr/local/bin/docker login -u \$HUB_USER -p \$HUB_PASS && /usr/local/bin/docker push vonnetworking/springboot-test"
     }
-
-    sh "/usr/local/bin/docker push vonnetworking/springboot-test"
   }
-
+  post {
+    always {
+      echo 'One way or another, I have finished'
+      docker rmi --force `docker images | grep springboot-test | head -1 | awk '{print \$3}'`
+      deleteDir() /* clean up our workspace */
+    }
 }

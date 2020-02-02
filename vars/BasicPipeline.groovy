706af4188b575738d2c60f00a4bff392b064c427
Av2 <vonnetworking2@gmail.com>
2020-02-01 20:38:16 -0500

    again with the docker build command syntax error

--- vars/BasicPipeline.groovy
+++ vars/BasicPipeline.groovy
@@ -10,7 +10,7 @@ def call(body) {
     def configMap = readYaml file:"pipeline_config.yaml"
     def app_version = readFile file:"VERSION"
     sh "echo 'App Version: $app_version'"
-    sh "/usr/local/bin/docker image build -t springboot-test:${app_version} ${workspace}"
+    sh "/usr/local/bin/docker image build -t springboot-test:${app_version} ${env.WORKSPACE}"
   }
   stage ("Docker Dev Push") {
     def app_version = readFile file:"VERSION"
